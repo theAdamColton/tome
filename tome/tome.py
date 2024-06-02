05ea600f0dffe942763752389d0ccfd8e89b7b84
Adam <atcolton@tutanota.com>
2024-06-02 10:51:46 -0600

    make no_grad a decorator

--- tome/tome.py
+++ tome/tome.py
@@ -28,6 +28,7 @@ class TokenMerger:
     unmerged_x = tm.unmerge(merged_x) # shape: batch_size, sequence_length, 64
     """
 
+    @torch.no_grad()
     def __init__(self, k: torch.Tensor, r: int, sequence_ids=None, mask_id=-100):
         """
         k: token embeddings, can be the key values from attention.
@@ -44,47 +45,44 @@ class TokenMerger:
         sequence_length = k.shape[1]
         assert sequence_length // 2 >= r
         assert r > 0
-        with torch.no_grad():
-            k = k / k.norm(dim=-1, keepdim=True)
-            # step 1.) Assign tokens to set A or set B
-            a, b = k[:, ::2, :], k[:, 1::2, :]
-            # step 2.) Draw one edge between each token in set A and the most similar token in set B
-            scores = einx.dot("b s1 z, b s2 z -> b s1 s2", a, b)
+        k = k / k.norm(dim=-1, keepdim=True)
+        # step 1.) Assign tokens to set A or set B
+        a, b = k[:, ::2, :], k[:, 1::2, :]
+        # step 2.) Draw one edge between each token in set A and the most similar token in set B
+        scores = einx.dot("b s1 z, b s2 z -> b s1 s2", a, b)
 
-            if sequence_ids is not None:
-                a_ids, b_ids = sequence_ids[:, ::2], sequence_ids[:, 1::2]
-                attention_mask = a_ids.unsqueeze(2) == b_ids.unsqueeze(1)
-                # scores where ids are not equal should be -inf
-                scores.masked_fill_(~attention_mask, torch.finfo(scores.dtype).min)
-                pad_mask = (a_ids == mask_id).unsqueeze(2) & (
-                    b_ids == mask_id
-                ).unsqueeze(1)
-                # makes pad tokens have high scores
-                scores.masked_fill_(pad_mask, torch.finfo(scores.dtype).max)
+        if sequence_ids is not None:
+            a_ids, b_ids = sequence_ids[:, ::2], sequence_ids[:, 1::2]
+            attention_mask = a_ids.unsqueeze(2) == b_ids.unsqueeze(1)
+            # scores where ids are not equal should be -inf
+            scores.masked_fill_(~attention_mask, torch.finfo(scores.dtype).min)
+            pad_mask = (a_ids == mask_id).unsqueeze(2) & (b_ids == mask_id).unsqueeze(1)
+            # makes pad tokens have high scores
+            scores.masked_fill_(pad_mask, torch.finfo(scores.dtype).max)
 
-            node_max, node_idx = einx.max("b s1 s2 -> b s1", scores)
-            edge_idx = node_max.argsort(dim=-1, descending=True)  # shape: b s 1
-            # step 3.) Keep the top r most similar edges
-            unm_idx = edge_idx[:, r:]  # Unmerged Tokens
-            src_idx = edge_idx[:, :r]  # Merged Tokens
-            dst_idx = node_idx.gather(dim=1, index=src_idx)  # shape: b r 1
+        node_max, node_idx = einx.max("b s1 s2 -> b s1", scores)
+        edge_idx = node_max.argsort(dim=-1, descending=True)  # shape: b s 1
+        # step 3.) Keep the top r most similar edges
+        unm_idx = edge_idx[:, r:]  # Unmerged Tokens
+        src_idx = edge_idx[:, :r]  # Merged Tokens
+        dst_idx = node_idx.gather(dim=1, index=src_idx)  # shape: b r 1
 
-            if sequence_ids is not None:
-                # Merges ids, but doesn't do any reduction on merged ids,
-                # Simply takes the items from set b.
-                # The reduction is uncessary because each set of merged ids is assumed to be identical,
-                # meaning that ids are not merged with ids that are different.
-                # An id 0 shouldn't be be merged with any id that is not 0.
-                unm_ids = a_ids.gather(dim=1, index=unm_idx)
-                a_ids = a_ids.gather(dim=1, index=src_idx)
-                assert torch.equal(
-                    b_ids,
-                    b_ids.scatter(1, dst_idx, a_ids),
-                ), "These ids should be equal. If this test fails it means that attention mask was not properly computed, \
-                and or tokens were incorrectly merged over sequence-id boundaries."
-                self.merged_ids = torch.cat((unm_ids, b_ids), 1)
-            else:
-                self.merged_ids = None
+        if sequence_ids is not None:
+            # Merges ids, but doesn't do any reduction on merged ids,
+            # Simply takes the items from set b.
+            # The reduction is uncessary because each set of merged ids is assumed to be identical,
+            # meaning that ids are not merged with ids that are different.
+            # An id 0 shouldn't be be merged with any id that is not 0.
+            unm_ids = a_ids.gather(dim=1, index=unm_idx)
+            a_ids = a_ids.gather(dim=1, index=src_idx)
+            assert torch.equal(
+                b_ids,
+                b_ids.scatter(1, dst_idx, a_ids),
+            ), "These ids should be equal. If this test fails it means that attention mask was not properly computed, \
+            and or tokens were incorrectly merged over sequence-id boundaries."
+            self.merged_ids = torch.cat((unm_ids, b_ids), 1)
+        else:
+            self.merged_ids = None
 
         self.unm_idx = unm_idx
         self.src_idx = src_idx
